bee90477d91deb89341073cc90366a2bd23fe47b
Loubout <louis.k.boutin@gmail.com>
2016-04-21 14:39:43 +0200

    started player spec and stuff

--- src/test/LemmingTestAvecContrat.java
+++ src/test/LemmingTestAvecContrat.java
@@ -19,17 +19,17 @@ public class LemmingTestAvecContrat {
 		GameEngContract gameEng = new GameEngContract(gameEngService);
 		LevelContract level = new LevelContract(levelService);
 
-		level.init(60, 10);
-		gameEng.init(10, 2);
+		level.init(100, 40);
+		gameEng.init(12, 2);
 		gameEng.bindLevel(level);
 		
 		System.out.println(gameEng.getLevel());
 		display.bindEngine(gameEng);
 		System.out.println("before goplay contract main");
 		level.goPlay();
-		int nbTours = 100;
+		//int nbTours = 100;
 		System.out.println("before for loop contract main");
-		for (int i = 0 ; i < nbTours; i++){
+		while(!gameEng.gameOver()){
 			gameEng.nextTurn();
 			display.displayLevel();
 			Thread.sleep(300);    
