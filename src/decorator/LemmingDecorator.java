cb7fc28121c7c914fd75074b122d6cdb8964c1c6
datng <nguyendatci@hotmail.com>
2016-04-23 17:51:16 +0200

    decorator

---
+++ src/decorator/LemmingDecorator.java
@@ -0,0 +1,68 @@
+package decorator;
+
+import enumeration.Direction;
+import enumeration.Status;
+import enumeration.Type;
+import services.GameEngService;
+import services.LemmingService;
+
+public class LemmingDecorator implements LemmingService {
+	
+	private LemmingService delegate;
+	
+	public LemmingDecorator(LemmingService delegate) {
+		this.delegate = delegate;
+	}
+
+	public Direction getDirection() {
+		return delegate.getDirection();
+	}
+
+	public int getNumber() {
+		return delegate.getNumber();
+	}
+
+	public Type getType() {
+		return delegate.getType();
+	}
+
+	public Status getStatus() {
+		return delegate.getStatus();
+	}
+
+	public int getX() {
+		return delegate.getX();
+	}
+
+	public int getY() {
+		return delegate.getY();
+	}
+
+	public int getFallTime() {
+		return delegate.getFallTime();
+	}
+
+	public GameEngService getGameEng() {
+		return delegate.getGameEng();
+	}
+
+	public void init(Type type, int num) {
+		delegate.init(num);
+	}
+
+	public void step() {
+		delegate.step();
+	}
+
+	
+	public void bindEngine(GameEngService eng) {
+		delegate.bindEngine(eng);
+	}
+
+	@Override
+	public void init(int num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
