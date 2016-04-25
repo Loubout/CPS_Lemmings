bdb39f43e5c1c444565c4db2a46a2d1da3106703
Loubout <louis.k.boutin@gmail.com>
2016-04-25 18:41:08 +0200

    gui

--- src/gui/GuiQT.java
+++ src/gui/GuiQT.java
@@ -12,37 +12,41 @@ import impl.LevelImpl;
 import services.LevelService;
 
 public class GuiQT {
-	
+
 	public GuiQT() throws InterruptedException{
 		DisplayImpl display = new DisplayImpl();
 		GameEngImpl gameEng = new GameEngImpl();
 		LevelService level = new LevelImpl();		
-		
-		
+
+
 		level.init(60, 10);
 		gameEng.init(10, 2);
 		gameEng.bindLevel(level);
-		
-		
+
 		Grid w = new Grid(null);
 		w.bindEngine(gameEng);
 		w.show();
 		
-	
-		
+
 		level.goPlay();
-		int nbTours = 100;
-		for (int i = 0 ; i < nbTours; i++){
-			gameEng.nextTurn();
-			w.repaint();
-			Thread.sleep(300);    
+		int nbTours = 50;
+		int cpt = 0;
+		while (cpt < nbTours){
+			if (!w.isPaused()){
+				gameEng.nextTurn();
+				w.repaint();
+				Thread.sleep(100); 
+				cpt++;
+			}
 		}
 	}
+
+
 	public static void main(String[] args) throws InterruptedException {
 		QApplication.initialize(args);
-		
+
 		new GuiQT();
-	
+
 		QApplication.execStatic();
 		QApplication.shutdown();
 	}
